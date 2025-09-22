098b4c6cdcb5af4141a35f4149d5412c02e27508
erfan-najafloo <erfan.najafloo@gmail.com>
2025-09-22 17:05:14 +0330

    initial sign-up form structure added

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,46 @@
+export function SignUpForm(){
+    return(
+        <>
+        <form action="">
+
+<img src="" alt="" />
+<p></p>
+<div>
+   
+  
+    
+    <input type="text" placeholder="نام" />
+    <input type="text" placeholder="نام خانوادگی"/>
+</div>
+<div>
+    <input type="text" placeholder="ایمیل"/>
+</div>
+<div>
+    <input type="text" placeholder="شرکت" />
+</div>
+<div>
+    <input type="text" placeholder="آدرس"/>
+</div>
+<div>
+    <input type="date" placeholder="ماه"/>
+    <input type="date" placeholder="روز"/>
+    <input type="date" placeholder="سال"/>
+</div>
+<div>
+    <input type="password" placeholder="رمز عبور"/>
+</div>
+<div>
+    <input type="password" placeholder="تکرار رمز عبور"/>
+</div>
+<button>
+    Sign Up
+</button>
+
+
+
+
+        </form>
+        
+        </>
+    )
+}
