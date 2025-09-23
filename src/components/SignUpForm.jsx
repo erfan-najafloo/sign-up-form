5b3799350889a19874ddfe1faa54fce655c63185
erfan-najafloo <erfan.najafloo@gmail.com>
2025-09-23 10:46:35 +0330

    feat(form): add Persian date picker for birth date

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
-
+import DatePicker from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
 export function SignUpForm() {
     const [formData, setFormData] = useState({
         firstName: "",
@@ -10,9 +12,10 @@ export function SignUpForm() {
         password: "",
         confirmPassword: "",
     });
-    const [birthDate, setBirthDate] = useState("");
+    const [birthDate, setBirthDate] = useState(null);
     function handleSubmit(event) {
         event.preventDefault();
+
     }
     return (
         <>
@@ -20,45 +23,46 @@ export function SignUpForm() {
                 <img src="" alt="" />
                 <p></p>
                 <div>
-                    <input type="text" placeholder="نام" value={formData.firstName} onChange={(event) => {
+                    <input type="text" placeholder=" نام خود را وارد کنید" value={formData.firstName} onChange={(event) => {
                         setFormData({ ...formData, firstName: event.target.value });
                     }} />
-                    <input type="text" placeholder="نام خانوادگی" value={formData.lastName} onChange={(event) => {
+                    <input type="text" placeholder="نام خانوادگی خود را وارد کنید" value={formData.lastName} onChange={(event) => {
                         setFormData({ ...formData, lastName: event.target.value });
 
                     }} />
                 </div>
                 <div>
-                    <input type="text" placeholder="ایمیل" value={formData.email} onChange={(event) => {
+                    <input type="text" placeholder="ایمیل خود را وارد کنید" value={formData.email} onChange={(event) => {
                         setFormData({ ...formData, email: event.target.value });
                     }} />
                 </div>
                 <div>
-                    <input type="text" placeholder="شرکت" value={formData.company} onChange={(event) => {
+                    <input type="text" placeholder="نام شرکت خود را وارد کنید" value={formData.company} onChange={(event) => {
                         setFormData({ ...formData, company: event.target.value });
                     }} />
                 </div>
                 <div>
-                    <input type="text" placeholder="آدرس" value={formData.address} onChange={(event) => {
+                    <input type="text" placeholder="آدرس خود را وارد کنید" value={formData.address} onChange={(event) => {
                         setFormData({ ...formData, address: event.target.value });
 
                     }} />
                 </div>
-                <input
-                        type="date"
-                        value={birthDate}
-                        onChange={(event)=>{
-                            setBirthDate(event.target.value);
-                        }}
-                    />
+                <DatePicker
+                    value={birthDate}
+                    onChange={setBirthDate}
+                    calendar={persian}
+                    locale={persian_fa}
+                    placeholder="تاریخ تولد خود را وارد کنید"
+                />
+
                 <div>
-                    <input type="password" placeholder="رمز عبور" value={formData.password} onChange={(event) => {
+                    <input type="password" placeholder="رمز عبور خود را ووارد کنید" value={formData.password} onChange={(event) => {
                         setFormData({ ...formData, password: event.target.value });
 
                     }} />
                 </div>
                 <div>
-                    <input type="password" placeholder="تکرار رمز عبور" value={formData.confirmPassword} onChange={(event) => {
+                    <input type="password" placeholder="تکرار رمز عبور خود را وارد کنید" value={formData.confirmPassword} onChange={(event) => {
                         setFormData({ ...formData, confirmPassword: event.target.value });
 
                     }} />
